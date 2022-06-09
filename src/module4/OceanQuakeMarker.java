57272c807c69e4e697fb0a9bd81eba36e3a5dea9
Shuja <shuja@email.com>
2022-06-09 22:22:44 +0200

    To be continued

---
+++ src/module4/OceanQuakeMarker.java
@@ -0,0 +1,49 @@
+package module4;
+
+import de.fhpotsdam.unfolding.data.PointFeature;
+import processing.core.PGraphics;
+
+/** Implements a visual marker for ocean earthquakes on an earthquake map
+ * 
+ * @author UC San Diego Intermediate Software Development MOOC team
+ * @author Your name here
+ *
+ */
+public class OceanQuakeMarker extends EarthquakeMarker {
+	
+	public OceanQuakeMarker(PointFeature quake) {
+		super(quake);
+		
+		// setting field in earthquake marker
+		isOnLand = false;
+	}
+	
+
+	@Override
+	public void drawEarthquake(PGraphics pg, float x, float y) {
+		// Drawing a centered square for Ocean earthquakes
+		// DO NOT set the fill color.  That will be set in the EarthquakeMarker
+		// class to indicate the depth of the earthquake.
+		// Simply draw a centered square.
+		
+		// HINT: Notice the radius variable in the EarthquakeMarker class
+		// and how it is set in the EarthquakeMarker constructor
+		
+		// TODO: Implement this method
+		String time="Past Hour";
+		float radius=1.75f*super.getMagnitude();
+		//pg.fill(255, 255, 255);
+		pg.rect(x,y,radius,radius);
+		if(time.equals((String)getProperty("age"))){
+			pg.fill(0, 0, 0);
+			pg.textSize(12);
+			pg.text("X",x,y);
+		}
+		
+	}
+	
+
+
+	
+
+}
